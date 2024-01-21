15cf080b3e0182cc948e8c898ec2748d4e3870b8
Flavien Solt <flsolt@ethz.ch>
2024-01-21 12:49:36 +0100

    Update top.sv

--- top.sv
+++ top.sv
@@ -1,13 +1,20 @@
 module top(clkin_data, in_data, out_data);
-  reg celloutsig_0z;
-  input [127:0] clkin_data;
-  wire [127:0] clkin_data;
+  wire celloutsig_0z;
+  wire celloutsig_1z;
+  wire [1:0] celloutsig_2z;
+  reg [1:0] celloutsig_3z;
+  input [95:0] clkin_data;
+  wire [95:0] clkin_data;
   input [95:0] in_data;
   wire [95:0] in_data;
   output [95:0] out_data;
   wire [95:0] out_data;
-  always_ff @(negedge clkin_data[0], posedge clkin_data[64], posedge clkin_data[96])
-    if (clkin_data[96]) celloutsig_0z[0] <= 1'b0;
-    else if (clkin_data[64]) celloutsig_0z[0] <= 1'b1;
-  assign out_data[0] = celloutsig_0z;
+  assign celloutsig_2z = in_data[69:68] & { in_data[68], celloutsig_0z };
+  assign celloutsig_0z = ~^ in_data[25:24];
+  assign celloutsig_1z = ~ celloutsig_0z;
+  always_ff @(negedge clkin_data[0], posedge clkin_data[32], negedge clkin_data[64])
+    if (!clkin_data[64]) celloutsig_3z[0] <= 1'b0;
+    else if (clkin_data[32]) celloutsig_3z[0] <= 1'b1;
+    else celloutsig_3z[0] <= celloutsig_0z;
+  assign { out_data[33:32], out_data[1:0] } = { celloutsig_2z, celloutsig_3z };
 endmodule
